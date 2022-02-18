4ab679e1058b856b88f42035adbbe4d0a642e590
Paul BERNARD <pbernard007@ensc.fr>
2022-02-18 22:43:39 +0100

    modifications sommaires

--- www/index.js
+++ www/index.js
@@ -2,20 +2,30 @@ import {memory} from "code-musique/code_musique_bg";
 import * as wasm from "code-musique";
 
 const codeArea = document.getElementById("codeArea");
-//const volumeSlider = document.getElementById("volumeSlider");
-let audioContext = new AudioContext();
+const playBtn = document.getElementById("playBtn");
 
-// volumeSlider.addEventListener("change", (event) => {
-//     const newVolumeValue = event.target.valueAsNumber;
-//     console.log(newVolumeValue);
-// })
-let a = 3;
-codeArea.addEventListener("keyup", (event)=> {
+let audioContext = new AudioContext();
+let bufferSourceNode = new AudioBufferSourceNode(audioContext);
+bufferSourceNode.connect(audioContext.destination);
+
+
+let audioBuffer = new AudioBuffer({
+    length:3*44000,
+    numberOfChannels:1,
+    sampleRate:44000,  
+});
+bufferSourceNode.buffer = audioBuffer;
+
+codeArea.addEventListener("input", (event)=> {
     console.log(event.target.value);
     let time0 = Date.now();
     let result = wasm.compile(event.target.value);
     console.log(Date.now() - time0);
-    let memoire = new Float64Array(memory.buffer, result.pointer, result.size);
-    
-    console.log(memoire);
-})
+    let memoire = new Float32Array(memory.buffer, result.pointer, result.size);
+    audioBuffer.copyToChannel(memoire,0);
+
+});
+
+playBtn.addEventListener("click", (event) => {
+    bufferSourceNode.start();
+});
